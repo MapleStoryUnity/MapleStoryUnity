7a562f69eea5d527639bc4c81e2ff6cd168443ee
Jen-Chieh Shen <jcs090218@gmail.com>
2022-07-07 01:17:17 +0800

    style(enum): replace tabs with spaces

--- Assets/_Project/Scripts/Network/Enum/SendPacketType.cs
+++ Assets/_Project/Scripts/Network/Enum/SendPacketType.cs
@@ -20,68 +20,68 @@
 
 namespace MSU
 {
-	/// <summary>
-	/// A list of packets to be sent to the server.
-	/// </summary>
-	public enum SendPacketType
+    /// <summary>
+    /// A list of packets to be sent to the server.
+    /// </summary>
+    public enum SendPacketType
     {
-		// GENERIC
-		PONG = 24,
+        // GENERIC
+        PONG = 24,
 
-		// Login
-		LOGIN = 1,
-		SERVERLIST_REREQUEST = 4,
-		CHARLIST_REQUEST = 5,
-		STATUS_REQUEST = 6,
-		ACCEPT_TOS = 7,
-		SET_GENDER = 8,
-		AFTER_LOGIN = 9,
-		REGISTER_PIN = 10,
-		SERVERLIST_REQUEST = 11,
-		SELECT_CHAR = 19,
-		PLAYER_LOGIN = 20,
-		NAME_CHAR = 21,
-		CREATE_CHAR = 22,
-		DELETE_CHAR = 23,
-		REGISTER_PIC = 29,
-		SELECT_CHAR_PIC = 30,
+        // Login
+        LOGIN = 1,
+        SERVERLIST_REREQUEST = 4,
+        CHARLIST_REQUEST = 5,
+        STATUS_REQUEST = 6,
+        ACCEPT_TOS = 7,
+        SET_GENDER = 8,
+        AFTER_LOGIN = 9,
+        REGISTER_PIN = 10,
+        SERVERLIST_REQUEST = 11,
+        SELECT_CHAR = 19,
+        PLAYER_LOGIN = 20,
+        NAME_CHAR = 21,
+        CREATE_CHAR = 22,
+        DELETE_CHAR = 23,
+        REGISTER_PIC = 29,
+        SELECT_CHAR_PIC = 30,
 
-		// Gameplay 1
-		CHANGEMAP = 38,
-		MOVE_PLAYER = 41,
-		CLOSE_ATTACK = 44,
-		RANGED_ATTACK = 45,
-		MAGIC_ATTACK = 46,
-		TAKE_DAMAGE = 48,
+        // Gameplay 1
+        CHANGEMAP = 38,
+        MOVE_PLAYER = 41,
+        CLOSE_ATTACK = 44,
+        RANGED_ATTACK = 45,
+        MAGIC_ATTACK = 46,
+        TAKE_DAMAGE = 48,
 
-		// Messaging
-		GENERAL_CHAT = 49,
+        // Messaging
+        GENERAL_CHAT = 49,
 
-		// Npc Interaction
-		TALK_TO_NPC = 58,
-		NPC_TALK_MORE = 60,
-		NPC_SHOP_ACTION = 61,
+        // Npc Interaction
+        TALK_TO_NPC = 58,
+        NPC_TALK_MORE = 60,
+        NPC_SHOP_ACTION = 61,
 
-		// Inventory
-		GATHER_ITEMS = 69,
-		SORT_ITEMS = 70,
-		MOVE_ITEM = 71,
-		USE_ITEM = 72,
-		SCROLL_EQUIP = 86,
+        // Inventory
+        GATHER_ITEMS = 69,
+        SORT_ITEMS = 70,
+        MOVE_ITEM = 71,
+        USE_ITEM = 72,
+        SCROLL_EQUIP = 86,
 
-		// Player
-		SPEND_AP = 87,
-		SPEND_SP = 90,
+        // Player
+        SPEND_AP = 87,
+        SPEND_SP = 90,
 
-		// Skill
-		USE_SKILL = 91,
+        // Skill
+        USE_SKILL = 91,
 
-		// Gameplay 2
-		PARTY_OPERATION = 124,
-		MOVE_MONSTER = 188,
-		PICKUP_ITEM = 202,
+        // Gameplay 2
+        PARTY_OPERATION = 124,
+        MOVE_MONSTER = 188,
+        PICKUP_ITEM = 202,
 
-		// Custom
-		HASH_CHECK = 30000,
-	}
+        // Custom
+        HASH_CHECK = 30000,
+    }
 }
