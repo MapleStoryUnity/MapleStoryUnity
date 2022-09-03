b32574556cdbca1f0175fd3488bc9e6d026ceea2
Jen-Chieh Shen <jcs090218@gmail.com>
2022-09-04 02:30:54 +0800

    chore(JCSUnity): update dep

---
+++ Assets/JCSUnity/Scripts/UI/Text/JCS_TextSliderDisplay.cs
@@ -0,0 +1,50 @@
+/**
+ * $File: JCS_TextSliderDisplay.cs $
+ * $Date: 2022-09-04 01:10:40 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2022 by Shen, Jen-Chieh $
+ */
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace JCSUnity
+{
+    /// <summary>
+    /// Text that display slider's value.
+    /// </summary>
+    public class JCS_TextSliderDisplay : JCS_TextObject
+    {
+        /* Variables */
+
+        [Header("** Initialize Variables (JCS_TextSliderDisplay) **")]
+
+        [Tooltip("To update the text along with this slider's value.")]
+        [SerializeField]
+        private Slider mSlider = null;
+
+        [Tooltip("Place you want to round the decimal.")]
+        [SerializeField]
+        [Range(0, 15)]
+        private int mRoundPlace = 2;
+
+        /* Setter & Getter */
+
+        public Slider slider { get { return this.mSlider; } set { this.mSlider = value; } }
+        public int RoundPlace { get { return this.mRoundPlace; } set { this.mRoundPlace = value; } }
+
+        /* Functions */
+
+        private void Update()
+        {
+            if (mSlider == null)
+                return;
+
+            double val = Math.Round(mSlider.value, mRoundPlace);
+
+            this.text = val.ToString();
+        }
+    }
+}
