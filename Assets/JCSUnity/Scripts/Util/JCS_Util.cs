32e2fd273368a7a9d817b29798540d4055455802
JenChieh <jcs090218@gmail.com>
2025-04-28 03:41:29 -0700

    feat: Update JCSUnity

--- Assets/JCSUnity/Scripts/Util/JCS_Util.cs
+++ Assets/JCSUnity/Scripts/Util/JCS_Util.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Text;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using PeterVuorela.Tweener;
 
@@ -1301,6 +1302,52 @@ namespace JCSUnity
         #endregion
 
         #region Audio
+
+        /// <summary>
+        /// Return a float value from the audio mixer.
+        /// </summary>
+        public static float GetFloat(AudioMixer mixer, string parameter)
+        {
+            float val;
+            mixer.GetFloat(parameter, out val);
+            return val;
+        }
+
+        /// <summary>
+        /// Convert volume level to decibel.
+        /// </summary>
+        public static float Volume2Decibel(float volume)
+        {
+            return Mathf.Log10(volume) * 20;
+        }
+
+        /// <summary>
+        /// Convert decibel to volume level.
+        /// </summary>
+        public static float Decibel2Volume(float dB)
+        {
+            return Mathf.Pow(10f, dB / 20f);
+        }
+
+        /// <summary>
+        /// Return the volume level from the audio mixer.
+        /// </summary>
+        public static float GetVolume(AudioMixer mixer, string parameter)
+        {
+            float dB = GetFloat(mixer, parameter);
+
+            return Decibel2Volume(dB);
+        }
+
+        /// <summary>
+        /// Set the audio mixer's volume.
+        /// </summary>
+        public static void SetVolume(AudioMixer mixer, string parameter, float volume)
+        {
+            float dB = Volume2Decibel(volume);
+
+            mixer.SetFloat(parameter, dB);
+        }
 
         /// <summary>
         /// Same with function `AudioSource.PlayClipAtPoint` with different
